7fffec211e4096b10ec7541bf50c2a79a9c77237
c-shah <chintanjshah@gmail.com>
2017-08-16 22:00:26 -0700

    auto

--- src/main/java/com/spring/heroku/services/EnvironmentService.java
+++ src/main/java/com/spring/heroku/services/EnvironmentService.java
@@ -15,10 +15,10 @@ public class EnvironmentService {
             environmentMap = new HashMap<String, String>();
             if( System.getenv().containsKey("DYNO") ) {
                 System.out.println(" getEnvironmentMap: we are in heroku environment ");
-                return System.getenv();
+                environmentMap = System.getenv();
             } else if( System.getenv().containsKey("ENV_FILE") ) {
                 System.out.println(" getEnvironmentMap: we are in heroku local ");
-                return System.getenv();
+                environmentMap = System.getenv();
             } else {
                 System.out.println(" getEnvironmentMap : we are in java run ");
                 File f = new File(".env");
